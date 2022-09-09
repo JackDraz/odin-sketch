1d7f70f7e72bc31aee003bf81574357b972e2683
JackDraz <jackwillburnett@gmail.com>
2022-09-09 14:05:01 +0100

    Adding variable canvas evt listener

--- script.js
+++ script.js
@@ -4,20 +4,23 @@
 var slider = document.getElementById("mySlider");
 var output = document.getElementById("scale");
 output.innerText = `${slider.value} x ${slider.value}`;
+let num = slider.value;
 
 slider.oninput = function() {
-    output.innerText = `${this.value} x ${this.value}`; 
+    output.innerText = `${this.value} x ${this.value}`;
+    return num = this.value;
 }
 
-let number = slider.value;
+
 let gridCanvas = document.getElementById("grid");
 let gridCanvasStyle = getComputedStyle(gridCanvas);
-const gridWidth = (parseInt(gridCanvasStyle.width));
+const canvasWidth = (parseInt(gridCanvasStyle.width));
 
 function createGrid(gridWidth, number) {
     let i = 1;
     while(i <= (number ** 2)) {
         const drawSquare = document.createElement("div");
+        drawSquare.classList.toggle("renderSquare");
         drawSquare.style.width = `${gridWidth/number}px`;
         drawSquare.style.height = `${gridWidth/number}px`;
         gridCanvas.appendChild(drawSquare);
@@ -25,4 +28,9 @@ function createGrid(gridWidth, number) {
     }
 }
 
-createGrid(gridWidth, 32);
+const input = document.querySelector(".submit-button");
+input.addEventListener("click", () => {
+    console.log(num);
+    gridCanvas.replaceChildren();
+    createGrid(canvasWidth, num);
+});
